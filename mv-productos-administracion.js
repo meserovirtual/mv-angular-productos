9af0d8231bc6cdd30d3b69ed056824f5d8c3950b
meserovirtual <meserovirtualarg@gmail.com>
2017-03-12 17:04:45 -0300

    Mateo
    Se agrego Pto de Reposición

--- mv-productos-administracion.js
+++ mv-productos-administracion.js
@@ -15,13 +15,13 @@
     }
 
     MvProductosController.$inject = ["ProductVars", 'ProductService', "MvUtils", "CategoryService", "UserService", "$scope",
-        "UploadVars", "UploadService", "ProductTypeService"];
+        "UploadVars", "UploadService", "ProductTypeService", 'MvUtilsGlobals', 'StockService', 'StockVars'];
     /**
      * @param AcProductos
      * @constructor
      */
     function MvProductosController(ProductVars, ProductService, MvUtils, CategoryService, UserService, $scope, UploadVars,
-                                   UploadService, ProductTypeService) {
+                                   UploadService, ProductTypeService, MvUtilsGlobals, StockService, StockVars) {
         var vm = this;
 
         vm.productos = [];
@@ -41,6 +41,12 @@
         vm.destacado = false;
         vm.status = true;
         vm.update = false;
+        vm.showProveedores = false;
+        vm.showProductoCompuesto = false;
+        vm.clear = true;
+        StockVars.clearCache = true;
+        vm.producto_para_kit = {};
+        vm.productos_en_kit = [];
 
 
         vm.foto_01 = {nombre: 'no_image.png'};
@@ -70,6 +76,7 @@
         vm.setCheckBox = setCheckBox;
 
 
+
         var element1 = angular.element(document.getElementById('nombre'));
         var element2 = angular.element(document.getElementById('precio_oferta'));
         var element3 = angular.element(document.getElementById('precio_oferta_desde'));
@@ -97,12 +104,15 @@
 
         function removeFocus() { }
 
+        document.getElementById('searchProducto').getElementsByTagName('input')[0].addEventListener('blur', function (event) {
+            console.log('busco');
+        });
+
         loadProductos();
 
         function loadProductos() {
             ProductService.get().then(function (data) {
                 setData(data);
-                console.log(data);
             });
         }
 
@@ -223,17 +233,17 @@
         }
 
         /*
-        $scope.$watch('$ctrl.producto', function () {
-            vm.listProveedores = [];
-            if (vm.producto.proveedores == undefined) {
-                return;
-            }
+         $scope.$watch('$ctrl.producto', function () {
+         vm.listProveedores = [];
+         if (vm.producto.proveedores == undefined) {
+         return;
+         }
 
-            for (var i in vm.producto.proveedores) {
-                vm.listProveedores[vm.producto.proveedores[i].usuario_id] = true;
-            }
-        });
-        */
+         for (var i in vm.producto.proveedores) {
+         vm.listProveedores[vm.producto.proveedores[i].usuario_id] = true;
+         }
+         });
+         */
 
         function save() {
             if(vm.producto.nombre == undefined || vm.producto.nombre.length == 0){
@@ -269,8 +279,10 @@
             vm.producto.en_slider = vm.en_slider ? 1 : 0;
             vm.producto.destacado = vm.destacado ? 1 : 0;
 
+            console.log(vm.producto);
+
             ProductService.save(vm.producto).then(function (data) {
-                //console.log(data);
+                console.log(data);
                 vm.detailsOpen = (data === undefined || data < 0) ? true : false;
                 if(data == undefined) {
                     element1[0].classList.add('error-input');
@@ -332,7 +344,7 @@
                 }
             }
 
-            //console.log(vm.producto);
+            console.log(vm.producto);
 
             if(vm.producto.producto_id === undefined) {
                 MvUtils.showMessage('error', 'Debe seleccionar un Producto');
@@ -354,9 +366,9 @@
                         vm.priceOpen = false;
                     }
                 })
-                .catch(function (data) {
-                    vm.producto = {};
-                });
+                    .catch(function (data) {
+                        vm.producto = {};
+                    });
             }
         }
 
@@ -391,6 +403,10 @@
         }
 
         function agregarKit() {
+            console.log(vm.producto.kits);
+            if(vm.producto.kits == undefined)
+                vm.producto.kits = [];
+
             vm.producto.kits.push({
                 producto_cantidad: 1,
                 producto_id: vm.producto_para_kit.producto_id,
@@ -403,7 +419,6 @@
         function quitarKit(producto_kit) {
             for (var i = 0; i < vm.producto.kits.length; i++) {
                 if (producto_kit.producto_id == vm.producto.kits[i].producto_id) {
-
                     vm.producto.kits.splice(i, 1);
                 }
             }
@@ -427,10 +442,12 @@
             } else {
                 var result = confirm('¿Esta seguro que desea eliminar el producto seleccionado?');
                 if(result) {
-                    ProductService.remove(vm.producto.producto_id, function(data){
+                    ProductService.remove(vm.producto.producto_id).then(function(data){
                         vm.producto = {};
                         cleanProducto();
                         loadProductos();
+                    }).catch(function(data){
+                        console.log(data);
                     });
                 }
             }
@@ -449,6 +466,7 @@
             cleanProducto();
             loadProductos();
         }
+
 
 
         // Implementación de la paginación
